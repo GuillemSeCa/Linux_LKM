b3f0f3afa7ac80dfed5735efbaf1342da5c21b2c
Guillem Serra Cazorla <44067226+GuillemSeCa@users.noreply.github.com>
2021-11-01 23:34:04 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -4,8 +4,7 @@ sudo apt install raspberrypi-kernel-headers
 
 if [ ! -d "Linux_LKM" ]; then
     echo "As the git repo doesn't exist already we clone it!"
-    sudo apt-get install git
-    git clone https://github.com/GuillemSeCa/Linux_LKM.git
+    sudo apt-get install git && git clone https://github.com/GuillemSeCa/Linux_LKM.git
 fi
 
 cd Linux_LKM && make clean && make && sudo insmod LKM.ko

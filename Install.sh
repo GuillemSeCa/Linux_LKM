127c0a6fe35e78616106f487c59bcc349e63bf68
GuillemSeCa <gserracazorla@gmail.com>
2021-11-01 21:42:39 +0000

    Improvement and Install script and Unistall script.

---
+++ Install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install raspberrypi-kernel-headers
+
+if [ ! -d "Linux_LKM" ]; then
+    echo "As the git repo doesn't exist already we clone it!"
+    git clone https://github.com/GuillemSeCa/Linux_LKM.git
+fi
+
+cd Linux_LKM && make clean && make && sudo insmod LKM.ko

3d94b6708dd809fea63ac2d7e002115714d8ecaf
Guillem Serra Cazorla <guillem.serra@students.salle.url.edu>
2021-11-04 15:02:01 +0100

    restructure of code

--- Install.sh
+++ Install.sh
@@ -10,5 +10,4 @@ if [ ! -d "code" ]; then
     echo "As the git repo doesn't exist already we clone it!"
     sudo apt-get -y install git && git clone https://github.com/GuillemSeCa/Linux_LKM.git
     cd Linux_LKM/code && make clean && make && sudo insmod LKM.ko && cp A.sh /home/pi/A.sh && cp B.sh /home/pi/B.sh && cp C.sh /home/pi/C.sh && cp D.sh /home/pi/D.sh && chmod +x /home/pi/A.sh /home/pi/B.sh /home/pi/C.sh /home/pi/D.sh
-    rm Install.sh
 fi

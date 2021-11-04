747e3e7d2c021a5ed1f4fba2f7d8fe32877bae37
Guillem Serra Cazorla <guillem.serra@students.salle.url.edu>
2021-11-04 14:55:28 +0100

    restructure of code

--- Install.sh
+++ Install.sh
@@ -11,5 +11,3 @@ if [ ! -d "code" ]; then
     sudo apt-get -y install git && git clone https://github.com/GuillemSeCa/Linux_LKM.git
     cd Linux_LKM/code && make clean && make && sudo insmod LKM.ko && cp A.sh /home/pi/A.sh && cp B.sh /home/pi/B.sh && cp C.sh /home/pi/C.sh && cp D.sh /home/pi/D.sh && chmod +x /home/pi/A.sh /home/pi/B.sh /home/pi/C.sh /home/pi/D.sh
 fi
-
-cd code && make clean && make && sudo insmod LKM.ko && cp A.sh /home/pi/A.sh && cp B.sh /home/pi/B.sh && cp C.sh /home/pi/C.sh && cp D.sh /home/pi/D.sh && chmod +x /home/pi/A.sh /home/pi/B.sh /home/pi/C.sh /home/pi/D.sh
